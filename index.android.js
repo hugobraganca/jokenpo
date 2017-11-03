7d0ba90f91e42e7eba4ebbf3ceb32531cedc8e65
Hugo Bragança da Silva <hugobraganca94@gmail.com>
2017-11-03 15:16:00 -0200

    update back end
    escolha da aplicação adicionada

--- index.android.js
+++ index.android.js
@@ -15,16 +15,34 @@ class jokenpo extends Component{
     }
 
     pedraPapelTesoura(escolhaUsuario){
-        this.setState({escolhaUsuario : escolhaUsuario});
+
+        //gera número aleatório (0, 1, 2)
+        var numAleatorio = Math.floor(Math.random() * 3);
+
+        var escolhaComputador = '';
+
+        var resultado = '';
+
+        switch(numAleatorio){
+            case 0: escolhaComputador = 'pedra'; break;
+            case 1: escolhaComputador = 'papel'; break;
+            case 2: escolhaComputador = 'tesoura'; break;
+        }
+        if(escolhaComputador == pedra && escolhaUsuario == pedra){
+            resultado = 'empate';
+        }else if()
+
+
+        this.setState({escolhaUsuario : escolhaUsuario, escolhaComputador : escolhaComputador});
 
     }
 
   render(){
     return(
         <View>
-            <Text>Escolha do computador</Text>
+            <Text>Escolha do computador: {this.state.escolhaComputador}</Text>
             <Text>Escolha do usuário: {this.state.escolhaUsuario}</Text>
-            <Text>Resuktado</Text>
+            <Text>Resultado</Text>
             <Button title="pedra" onPress={ () => {this.pedraPapelTesoura('pedra')}}/>
             <Button title="papel" onPress={ () => {this.pedraPapelTesoura('papel')}}/>
             <Button title="tesoura" onPress={ () => {this.pedraPapelTesoura('tesoura')}}/>
