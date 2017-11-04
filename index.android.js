a043af05a78c9601d19419e2973731b258f54a70
Hugo Bragança da Silva <hugobraganca94@gmail.com>
2017-11-04 01:25:22 -0200

    Imagens
    Adição de imagens e exibição das mesmas de acordo com o item selecionado pelo usuário e computador.

--- index.android.js
+++ index.android.js
@@ -4,7 +4,8 @@ import {
   StyleSheet,
   Text,
   Button,
-  View
+  View,
+  Image
 } from 'react-native';
 
 class jokenpo extends Component{
@@ -37,7 +38,7 @@ class jokenpo extends Component{
         }
         if((escolhaComputador == 'pedra' && escolhaUsuario == 'tesoura') || (escolhaComputador == 'papel' && escolhaUsuario == 'pedra') ||
             (escolhaComputador == 'tesoura' && escolhaUsuario == 'papel')){
-            resultado = 'você perdeu';
+            resultado = 'Você perdeu';
         }
         if( (escolhaUsuario == 'pedra' && escolhaComputador == 'tesoura') || (escolhaUsuario == 'papel' && escolhaComputador == 'pedra') ||
         (escolhaUsuario == 'tesoura' && escolhaComputador == 'papel')){
@@ -49,17 +50,101 @@ class jokenpo extends Component{
   render(){
     return(
         <View>
-            <Text>Escolha do computador: {this.state.escolhaComputador}</Text>
-            <Text>Escolha do usuário: {this.state.escolhaUsuario}</Text>
-            <Text>Resultado: {this.state.resultado}</Text>
-            <Button title="pedra" onPress={ () => {this.pedraPapelTesoura('pedra')}}/>
-            <Button title="papel" onPress={ () => {this.pedraPapelTesoura('papel')}}/>
-            <Button title="tesoura" onPress={ () => {this.pedraPapelTesoura('tesoura')}}/>
+
+            <Topo></Topo>
+
+            <View style={styles.painelAcoes}>
+
+                <View style={styles.btnEscolha}>
+                    <Button title="pedra" onPress={ () => {this.pedraPapelTesoura('pedra')}}/>
+                </View>
+                <View style={styles.btnEscolha}>
+                    <Button title="papel" onPress={ () => {this.pedraPapelTesoura('papel')}}/>
+                </View>
+                <View style={styles.btnEscolha}>
+                    <Button title="tesoura" onPress={ () => {this.pedraPapelTesoura('tesoura')}}/>
+                </View>
+
+            </View>
+
+            <View style={styles.palco}>
+                <Text style={styles.txtResultado}>{this.state.resultado}</Text>
+                <Icone escolha={this.state.escolhaUsuario} jogador='Você'></Icone>
+                <Icone escolha={this.state.escolhaComputador} jogador='Computador'></Icone>
+            </View>
         </View>
     );
   }
-
-
 }
+
+class Topo extends Component{
+    render(){
+        return(
+            <View>
+                <Image source = {require('./imgs/jokenpo.png')}/>
+            </View>
+        );
+    }
+}
+class Icone extends Component{
+    render(){
+        //this.props.escolha
+        //this.props.jogador
+
+        if(this.props.escolha == 'pedra'){
+            return(
+                <View style={styles.icone}>
+                    <Text style={styles.txtJogador}>{this.props.jogador}</Text>
+                    <Image source = {require('./imgs/pedra.png')}/>
+                </View>
+            );
+        }else if (this.props.escolha == 'papel'){
+            return(
+                <View style={styles.icone}>
+                    <Text style={styles.txtJogador}>{this.props.jogador}</Text>
+                    <Image source = {require('./imgs/papel.png')}/>
+                </View>
+            );
+
+        }else if(this.props.escolha == 'tesoura'){
+            return(
+                <View style={styles.icone}>
+                    <Text style={styles.txtJogador}>{this.props.jogador}</Text>
+                    <Image source = {require('./imgs/tesoura.png')}/>
+                </View>
+            );
+
+        }else{
+            return false;
+        }
+    }
+}
+const styles = StyleSheet.create({
+    btnEscolha: {
+        width: 90
+    },
+    painelAcoes: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginTop: 10
+    },
+    palco: {
+        alignItems: 'center',
+        marginTop: 10,
+    },
+    txtResultado: {
+        fontSize: 25,
+        fontWeight: 'bold',
+        color: 'red',
+        height: 60
+    },
+    icone: {
+        alignItems: 'center',
+        marginBottom: 20
+    },
+    txtJogador: {
+        fontSize: 18
+    }
+});
 
 AppRegistry.registerComponent('jokenpo', () => jokenpo);
